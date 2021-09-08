e4288922b9f0dfb7b03332fdbc5e95adae398ae1
john <john@dailykos.com>
2021-09-08 12:34:57 -0700

    Use games model in models.py file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -43,21 +43,7 @@ def create_app(test_config=None):
 app = create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{app.config['DATABASE_USER']}:{app.config['DATABASE_PASSWORD']}@localhost/{app.config['DATABASE_NAME']}"
 db = SQLAlchemy(app)
-class GamesModel(db.Model):
-    __tablename__ = 'games'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
-    author = db.Column(db.String())
-    image_url = db.Column(db.String())
-
-    def __init__(self, name, author, image_url):
-        self.name     = name
-        self.author   = author
-        self.image_url = image_url
-
-    def __repr__(self):
-        return f"<Game {self.name}>"
 migrate = Migrate(app, db)
 
 # a simple page that says hello
