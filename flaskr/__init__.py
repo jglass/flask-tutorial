143f30c9c7e3033fd5caf722d25c7a7457969d6e
john <john@dailykos.com>
2021-09-08 14:18:14 -0700

    Remove cars model

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -85,32 +85,3 @@ def handle_games():
             } for game in games]
 
         return render_template('games.html', games=results)
-
-
-
-
-
-
-# Imports and CarsModel truncated
-# @app.route('/cars', methods=['POST', 'GET'])
-# def handle_cars():
-#     if request.method == 'POST':
-#         if request.is_json:
-#             data = request.get_json()
-#             new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
-#             db.session.add(new_car)
-#             db.session.commit()
-#             return {"message": f"car {new_car.name} has been created successfully."}
-#         else:
-#             return {"error": "The request payload is not in JSON format"}
-#
-#     elif request.method == 'GET':
-#         cars = CarsModel.query.all()
-#         results = [
-#             {
-#                 "name": car.name,
-#                 "model": car.model,
-#                 "doors": car.doors
-#             } for car in cars]
-#
-#         return {"count": len(results), "cars": results}
