32d6c281f08b6f90571ceb263e49e076d439d770
john <john@dailykos.com>
2021-10-15 13:50:23 -0700

    Add user model for login

--- flaskr/models.py
+++ flaskr/models.py
@@ -24,3 +24,17 @@ class GameModel(db.Model):
 
     def __repr__(self):
         return f"<Game {self.name}>"
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id       = db.Column(db.Integer, primary_key=True)
+    email    = db.Column(db.String())
+    password = db.Column(db.String())
+
+    def __init__(self, email, password):
+        self.email    = email
+        self.password = password
+
+    def __repr__(self):
+        return f"<User {self.email}>"
