d1200fecff9b01dd062a6a14ebd59079101fca00
john <john@dailykos.com>
2021-11-03 16:28:31 -0700

    Rename model test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,13 +3,10 @@ import pytest
 from flaskr import create_app
 from flaskr import GameModel
 
-model = GameModel(name="Thirsty Sword Lesbians",
+game_model = GameModel(name="Thirsty Sword Lesbians",
                   author="April Kit Walsh",
                   image_url="https://img.itch.zone/aW1nLzYxMjUyODYuanBn/347x500/Bnmn5s.jpg",
                   type="TTRPG")
 
 def test_gamemodel():
-    assert isinstance(model, GameModel)
-
-def test_boolean():
-    assert model.active is True
+    assert isinstance(game_model, GameModel)
