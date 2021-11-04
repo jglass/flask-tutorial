497cbe5493f507dd988456134c8b7723b930514b
john <john@dailykos.com>
2021-11-03 17:29:38 -0700

    Add routes test

---
+++ tests/test_routes.py
@@ -0,0 +1,19 @@
+from flask import request
+import pytest
+import pdb
+
+from flaskr import app
+from flaskr import GameModel
+
+game_model = GameModel(name="Thirsty Sword Lesbians",
+                  author="April Kit Walsh",
+                  image_url="https://img.itch.zone/aW1nLzYxMjUyODYuanBn/347x500/Bnmn5s.jpg",
+                  type="TTRPG")
+
+def test_game_entry_post():
+    with app.test_client() as test_client:
+        response = test_client.post('/game_entry',  data=dict(name="Thirsty Sword Lesbians",
+                                                    author="April Kit Walsh",
+                                                    image_url="https://img.itch.zone/aW1nLzYxMjUyODYuanBn/347x500/Bnmn5s.jpg",
+                                                    type="TTRPG"))
+        assert response.status_code == 302
